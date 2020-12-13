cb1e137687880a98f382f8f939b5679554576173
Assa583 <72857123+Assa583@users.noreply.github.com>
2020-12-13 12:27:44 +0100

    Add files via upload

--- python_projet_modele.py
+++ python_projet_modele.py
@@ -19,14 +19,14 @@ import numpy as np
 import statsmodels.tsa.api as smt
 import statsmodels.api as sm
 from statsmodels.tsa.stattools import adfuller
-pip install arch
-from arch import arch_model
 import itertools
+from itertools import product
 import sys
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 import os
 import math
+import warnings
 
 # I - Aperçu du Data Frame
 # II - Analyse du data frame
@@ -117,28 +117,51 @@ fig.update_layout(title=" Trajectoire représentant le prix à la cloture selon 
 #l'autocorrélation empirique estimera très mal l'autocorrélation. Par conséquent, on interprétera mal l'ACF. 
 
 #Le prix à la clôture n'est pas stationnaire car on pouvait observer un phénomène de tendance.
-#Représentons le graphe de moyenne mobile, de l'ec-type mobile et de la série pour confirmer cela.
+#Représentons le graphe de moyenne mobile, de l'ec-type mobile et de la série et faisons un test de D-F pour confirmer cela.
 
-rolling_mean = df_coin.close.rolling(window = 25).mean()
-rolling_std = df_coin.close.rolling(window = 25).std()
-plt.plot(df_coin.close, color = 'blue', label = 'Données brutes')
-plt.plot(rolling_mean, color = 'red', label = 'Moyenne mobile')
-plt.plot(rolling_std, color = 'black', label = 'Ecart-type mobile')
-plt.legend(loc = 'best')
-plt.title('Moyenne et Ecart-type mobiles')
-plt.show()
+def stationarity(timeseries):
+    
+    # Statistiques mobiles
+    rolling_mean = timeseries.rolling(window=25).mean() #moyenne mobile
+    rolling_std = timeseries.rolling(window = 25).std() #ecartype-mobile
+    
+    # tracé statistiques mobiles
+    originale = plt.plot(timeseries, color='blue', label='Données brutes')
+    mean = plt.plot(rolling_mean, color='red', label='Moyenne Mobile') #tendance estimé par MA
+    ec_type = plt.plot(rolling_std, color = 'black', label = 'Ecart-type mobile')
+    plt.legend(loc='best') 
+    plt.title('Moyenne et Ecartype mobile')
+    plt.show(block=False)
+    
+    # Test Dickey–Fuller : 
+    result = adfuller(timeseries)
+    print('Statistiques ADF : {}'.format(result[0]))
+    print('p-value : {}'.format(result[1]))
+    print('Valeurs Critiques :')
+    for key, value in result[4].items():
+        print('\t{}: {}'.format(key, value))
 
 
+
+stationarity(df_coin["close"]) 
+
+
+#Graphique Moyenne et Ecartype Mobile
 #On observe que la moyenne mobile n'est pas constante, elle augmente ou baisse avec le temps, bien que l'ec-type mobile reste plus ou moins constant
-#Ce qui nous confirme que la série n'est pas stationnaire.
 
+#Test de Dickey-Fuller
+#Le test de Dickey-Fuller est définit comme suit; H0 :" La série n'est pas stationnaire" vs H1= "La série est stationnaire"
+#On obtient une p-value de 16% donc on rejete H0 
+
+#Ce qui nous confirme que la série n'est pas stationnaire.
 #l'ACF de la série chronologique
+
 
 from statsmodels.graphics.tsaplots import plot_acf 
 plot_acf (df_coin.close, lags = 90)
 
 # Nous voyons bien toutes les limites de l'ACF. En effet, nous ne pouvons pas conclure sur le fait que les variables de la série soient indépendantes dès lors qu'il y a un phénomène de tendance qui apparaît
-# Le but est  de retirer ces phénomènes de tendance et de saisonnalité afin de pouvoir estimer le cours de clôture.
+
 
 ###########################################################################################################################
 
@@ -148,7 +171,7 @@ plot_acf (df_coin.close, lags = 90)
 # Rappel : volatilité d'une action = propension à subir des mouvements de prix plus ou moins prononcés.
 # A priori,si on suppose qu'on achète l'actif en Mars 2019, on pourrait penser que le meilleur moment pour vendre notre actif serait aux alentours de fin Juin 2019 
 #car le prix de clôture atteint son maximum, par conséquent on pourrait  réalisé un grand bénéfice ( de 15M de dollars ).
-# (Et à contrario, le moment le moins opportun durant la période observé, serait en Mars 2020  car ferait seulement un bénéfice de 2M .
+# Et à contrario, le moment le moins opportun durant la période observé, serait en Mars 2020  car ferait seulement un bénéfice de 2M .
 #Voyons cela de plus près...
 
 #On calcule les log-rendements de la série entre le prix_t et t-1
@@ -175,54 +198,61 @@ fig.update_layout(title=" Trajectoire représentant le log-rendement selon le te
 
 
 #Commentaire : 
-
-#Le log-rendement (mesure de la volatilité) tourne plus ou moins autour de 5%/7%, l'actif est globalement peu volatile.
+#Ce qu'on observe : 
+#Le log-rendement tourne plus ou moins autour de 5%/7%, l'actif est globalement peu volatile.
 #Cependant, on observe des pics ; il y a une très forte volatilité de l'actif autour du mois de Mars 2020 ( environ 19%), donc le risque qu'encourt l'investisseur est important à ce moment.
 #On observe également un autre pic en fin Juin 2019, car le cours atteint son maximum le 26 Juin 2020 et ensuite chute le 27 Juin 2020.
-#Ce qui rejoint ce que nous avions dit précédemment à savoir que plus l'amplitude des variations du prix est important, plus
-#l'actif sera volatile donc plus l’espérance de gain (ou risque de perte) sera important.
-
-
+#Cependant, on se rend compte que le signal qu'on observe nous apporte pas énormément d'informations .
+#En effet, la série des log-rendement étant très bruitée, on ne peut pas isoler des tendances pour avoir une stratégie de vente.
+#Nous allons donc par la suite, travailler uniquement avec le prix de clôture ( close-price)
 
 ######################################### III- Estimation paramétrique  ###################################################
 
 ###########################################################################################################################
 
-#A ) Estimation paramétrique du log-rendement 
+#A ) Estimation paramétrique du price-close avec différents processus.
 
-# PREDICTION AVEC L'ALGORITHME ARIMA #
+#  ESTIMATION AVEC L'ALGORITHME ARIMA 
 
-#Pq ce choix d'algortihme ? L'algo ARIMA permet de faire des prédictions même si la série est non -stationnaire
+#Pq ce choix d'algortihme ? Le modèle ARIMA permet de faire des prédictions même si la série est non -stationnaire
 
-# Explication : ARIMA est la combinaison de trois termes : 
+#  ARIMA est la combinaison de trois termes : 
 # le terme autorégressif (AR), le terme de différenciation (I) et le terme de moyennes mobiles (MA)
 # ARIMA (p,d,q):
 # p est le nombre de termes auto-régressifs
 # d est le nombre de différences
 # q est le nombre de moyennes mobiles
 
-# order=() ce sont respectivement les coefficients AR, I et MA je test le modèle avec order=( 1,1,1) 
-mdl = sm.tsa.statespace.SARIMAX(logR,order=(1, 1, 1))
+# order=() ce sont respectivement les coefficients AR, I et MA je test le modèle avec p=1 , d=1 et q=1 
+mdl = sm.tsa.statespace.SARIMAX(df_coin["close"],order=(1, 1, 1))
 res = mdl.fit()
 print(res.summary())
 
 #Observations : On constate que 3 coefficients ont été estimés :
-# 1 coefficient du terme auto-régressif : -0.0594
-# le terme de moyenne mobile : -0.9998
-# et la variance du bruit :  0.6241   
+# 1 coefficient du terme auto-régressif : 0.3990
+# le terme de moyenne mobile : -0.4656
+# et la variance du bruit :  9640.3804   
+
+#Les équartypes estimés des estimateurs de ces coefficients sont les suivants 
+# pour le terme AR 0.039, 
+#pour le terme MA 0.037, 
+#pour la variance du bruit 28.790
+
+#Les tests de student nous informent que les coefficients sont tous non-nuls donc qu'ils sont signifcatifs.
+
 
 #Graphiques de la modélisation
 res.plot_diagnostics(figsize=(16, 10))
 plt.tight_layout()
 plt.show()
 
-y = pd.DataFrame(logR)
+y = pd.DataFrame(df_coin["close"])
 
 
 
 # adapter le modèle aux données
 res = sm.tsa.statespace.SARIMAX(y,
-                                order=(2, 1, 2),
+                                order=(1, 1, 1),
                                 enforce_stationarity=True,
                                 enforce_invertibility=True).fit()
  
@@ -248,129 +278,80 @@ plt.show()
 rmse = math.sqrt(((pred.predicted_mean.values.reshape(-1, 1) - y[430:].values) ** 2).mean())
 print('rmse = '+ str(rmse))
 
-# rmse = 0.912
+#rmse = 99.48
 
 #Conclusion : 
-# On peut remarquer que la prédiction obtenue en utilisant l'algorithme ARIMA n'est pas du tout satisfaisante.
-#En effet, la prédiction rate presque tout le temps les piques du log-rendement, or ces piques sont très importants car cela nous
-#renseigne que la volatilité de l'actif financier est très forte (sûrement du à un Krach Boursier) donc que le risque
-#qu'encourt l'investisseur est grand à ce moment. De plus, on peut observer sur le graphe des résidus standardisés, que
-#les données du log-rendement sont trop bruitées et qu'il y beaucoup de volatilité dans les données.
-#Ce qui explique le fait que la prédiction soit inintéressante. 
-
-#Les ARMA ont une faiblesse dès lors qu'il y a de la volatilité dans les données.
-#Il existe un modèle pour modéliser les log-rendements d'une série financière et qui tient compte de la volatilité.
-#Ce sont les processus GARCH. 
-
-#Pour appliquer un modèle GARCH, la série doit être stationnaire.On sait que la série du prix à la clôture n'est pas stationnaire
-#Cependant, la série de du log-rendement l'est peut-être car en prenant le log d'une série on peut se ramener à qlq chose de stationnaire.
-
-#Vérifions si la série du log-rendement est stationnaire.
-
-def stationarity(timeseries):
-    
-    # Statistiques mobiles
-    rolling_mean = timeseries.rolling(window=12).mean() #moyenne mobile
-    
-    # tracé statistiques mobiles
-    original = plt.plot(timeseries, color='blue', label='Origine')
-    mean = plt.plot(rolling_mean, color='red', label='Moyenne Mobile') #tendance estimé par MA
-    plt.legend(loc='best') 
-    plt.title('Moyenne mobile')
-    plt.show(block=False)
-    
-    # Test Dickey–Fuller : 
-    result = adfuller(timeseries)
-    print('Statistiques ADF : {}'.format(result[0]))
-    print('p-value : {}'.format(result[1]))
-    print('Valeurs Critiques :')
-    for key, value in result[4].items():
-        print('\t{}: {}'.format(key, value))
+# On peut remarquer que l'estimation obtenue en utilisant l'algorithme ARIMA est très satisfaisante.
+# En effet, la courbe rouge ( estimation par ARIMA) superpose presque la courbe bleu ( trajectoire des données brutes).
+#On aimerait savoir si on peut améliorer cette modélisation.
+# Quel est le meilleur modèle pour modéliser la trajectoire de notre prix de clôture. 
+#Est-il préférable de la modéliser par un ARIMA(1,1,1)? ou par un ARIMA(1,1,3)? ou par un ARIMA(2,2,5)... ?
+#Nous allons considérer une famille M de tous les ARIMA, jusqu'à un certain ordre et on va essayer de trouver le meilleur modèle
+#pour modéliser cette série de données brutes en utilisant un critère BIC.
+#Pq le BIC et pas l'AIC ? Les deux permettent de minimiser les erreurs de prédictions mais l'AIC a un défaut celui de la suparamétrisation.
 
 
-logR_2=logR 
-logR_2.dropna(inplace=True) # car le test fonctionne si on enlève les valeurs NA
-get_stationarity(logR_2) 
+#SELECTION DE MODELE
 
-#Graphiquement : 
-#La moyenne mobile qui estime la tendance de la série logR est bien constante donc la série logR est stationnaire.
+# Initialisation 
+p= range(0, 6)
+q= range(0, 5)
+d=1
+parameters = product(p,q)
+parameters_list = list(parameters)
+len(parameters_list)
 
-#Résultat du test 
-#Le test de Dickey-Fuller est définit comme suit; H0 :" La série n'est pas stationnaire" vs H1= "La série est stationnaire"
-#On obtient une p-value de 0% donc on rejete H0 pour tout alpha. La serie du logR est bien stationnaire.
-#On peut appliquer le modèle GARCH à ces données de log rendement.
-
-garch = arch_model(logR, vol='GARCH', p=1, q=1)
-resultat = garch.fit()
-print(resultat.summary())
-
-
-
-#PB INSTALLATION ARCH 
-
-
-
-
-
-
-
-
-# Essayons uniquement de prendre en compte les prix à la clôture.
-
-# B ) Estimation paramétrique du cours de clôture 
-
-#Le prix à la clôture n'était  pas stationnaire car on pouvait observer un phénomène de tendance.
+# Selection de modèle
+results = []
+best_bic = float("inf")
+warnings.filterwarnings('ignore')
+for param in parameters_list:
+    try:
+        modele=sm.tsa.statespace.SARIMAX(df_coin["close"], order=(param[0], d, param[1])).fit()
+    except ValueError:
+        print('wrong parameters:', param)
+        continue
+    bic = modele.bic
+    if bic < best_bic:
+        best_modele = modele
+        best_bic = bic
+        best_param = param
+    results.append([param, modele.bic])
 
 
-#On peut tout de même faire le modèle ARIMA.
+# Affichage des résultats et du meilleur modèle
+result_table = pd.DataFrame(results) #on met les résultats dans un dataframe
+result_table.columns = ['parameters', 'bic']  #on souhaite uniquement afficher les colomnes des parametres et du critere BIC
+print(result_table.sort_values(by = 'bic', ascending=True).head()) # Affichage des premiers meilleurs modèles (qui ont le + petit BIC)
+print(best_modele.summary()) #affichage du meilleur modèle
 
-mdl2 = sm.tsa.statespace.SARIMAX(df_coin["close"],order=(0, 0, 0),seasonal_order=(2, 2, 1, 7))
-res2 = mdl2.fit()
-print(res2.summary())
+#Le meilleur modèle pour modéliser le prix de clôture est le modèle ARIMA(2, 1, 0).
 
-#Graphique des estimations
-res2.plot_diagnostics(figsize=(16, 10))
+#Analyse des résidus du modèle ARIMA(2,1,0) pour s'assurer que c'est un bon modèle  
+
+plt.figure(figsize=(15,7))
+plt.subplot(300)
+best_modele.resid[13:].plot()
+plt.ylabel(u'Residuals')
+ax = plt.subplot(212)
+sm.graphics.tsa.plot_acf(best_modele.resid[13:].values.squeeze(), lags=48, ax=ax)
+
+print("Dickey–Fuller test:: p=%f" % sm.tsa.stattools.adfuller(best_modele.resid[13:])[1])
+
 plt.tight_layout()
 plt.show()
 
-
-y = pd.DataFrame(df_coin["close"])
-res2 = sm.tsa.statespace.SARIMAX(y,
-                                order=(0, 0, 0),
-                                seasonal_order=(2, 2, 1, 7),
-                                enforce_stationarity=True,
-                                enforce_invertibility=True).fit()
- 
-
- #Limites de la prédiction
-pred = res2.get_prediction(start = 430,
-                         dynamic = False, 
-                          full_results=True)
-
-# Graphe de la prédiction et des données
-fig = plt.figure(figsize=(19, 7))
-ax = fig.add_subplot(111)
-ax.plot(y[0:],color='#006699',linewidth = 3, label='Observation');
-pred.predicted_mean.plot(ax=ax,linewidth = 3, linestyle='-', label='Prediction', alpha=.7, color='#ff5318', fontsize=18);
-ax.set_xlabel('temps', fontsize=18)
-ax.set_ylabel('logR', fontsize=18)
-plt.legend(loc='upper left', prop={'size': 20})
-plt.title('Prediction ARIMA', fontsize=22, fontweight="bold")
-plt.show()
-
-rmse = math.sqrt(((pred.predicted_mean.values.reshape(-1, 1) - y[430:].values) ** 2).mean())
-print('rmse = '+ str(rmse))
-
-#On constate que la qualité d'ajustement du modèle est nettement meilleure. 
-# L'algortihme a très bien estimé le prix de clôture.
-
-# Cependant, l'objectif de notre projet c'est surtout un choix d'investissement à prendre
-# La volatilité est donc un critère à prendre en compte .
+#On accepte que le modèle est bon pour modéliser la trajectoire du prix de clôture.
 
 
-#C )  Alogrithme LSTM permettant d'estimer le log-rendement.
+
+
+
+#C )  Alogrithme LSTM permettant d'estimer le close-price.
 
 ###########################################################################################################################
+
+
 
 
 
